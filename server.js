ad06896bffe6634e3772d8bd1ebfe2e4f09df6c3
Emily Aguirre <emgal.aguirre@gmail.com>
2020-11-23 10:14:38 -0500

    cleaned up server.js

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
-app.use(logger("dev"));
+//database connection
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+  })
+  .then((connection) => {
+    console.log("connection made");
+  });
+
+//middlewear
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
-const db = require("./models");
-//create routes js
 
 require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes/htmlRoutes")(app, __dirname);
 
 app.listen(PORT, () => {
-    console.log('App is running on port ${PORT}!');
+  console.log("App is running on port ${PORT}!");
 });
